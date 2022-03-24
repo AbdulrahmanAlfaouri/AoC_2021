e5f6994ee13e33232a8bd6cbd925fb645f5de957
Abdulrahman <abdulrahmanalfaouri@gmail.com>
2022-03-24 10:39:15 +0200

    for day 8

--- d6.py
+++ d6.py
@@ -1,32 +1,32 @@
-fshees = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0}
+fish_timers = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0}
 
 with open('input.txt') as in_text:
     in_nums = in_text.readline().split(',')
     in_nums = [int(i) for i in in_nums]
 
 for num in in_nums:
-    for key in fshees:
+    for key in fish_timers:
         if num == key:
-            fshees[key] += 1
+            fish_timers[key] += 1
 
 
 fishs = {6:0, 8:0}
 
 
 def shift():
-    fishs[6] = fshees[0]
-    fishs[8] = fshees[0]
+    fishs[6] = fish_timers[0]
+    fishs[8] = fish_timers[0]
 
-    for key in fshees:
+    for key in fish_timers:
         try:
-            fshees[key] = fshees[key + 1]
+            fish_timers[key] = fish_timers[key + 1]
         except KeyError:
             pass
 
 
 def addshit():
-    fshees[8] = fishs[8]
-    fshees[6] += fishs[6]
+    fish_timers[8] = fishs[8]
+    fish_timers[6] += fishs[6]
 
 
 for day in range(256):
@@ -34,6 +34,6 @@ for day in range(256):
     addshit()
 
 
-theSum = list(fshees.values())
+theSum = list(fish_timers.values())
 print(sum(theSum))
 
--- d7.py
+++ d7.py
@@ -2,4 +2,28 @@ with open('input.txt') as in_text:
     in_numbers = in_text.readline().split(',')
     in_numbers = [int(num) for num in in_numbers]
 
-# 99053143
+
+all_of_the_really_shits = []
+
+for i in range(min(in_numbers), max(in_numbers)):
+    print(f'using i = {i}')
+    really_shit = 0
+    for hehe in in_numbers:
+        shit = 0
+        full_shit = 0
+        m = 0
+        shit += abs(i-hehe)
+        for pshit in range(shit+1):
+            full_shit += pshit
+
+        really_shit += full_shit
+
+
+
+    all_of_the_really_shits.append(really_shit)
+
+try:
+    print(min(all_of_the_really_shits))
+except ValueError:
+    print('shit')
+
